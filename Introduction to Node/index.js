067e82d16eafe64c874586de38a33578bc83fb8e
Zik <zikrulla.zik@gmail.com>
2025-12-06 02:56:48 +0530

    Directory Operations

--- Introduction to Node/index.js
+++ Introduction to Node/index.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 
 
 // read file
-const data = fs.readFileSync('f1.txt')
+// const data = fs.readFileSync('f1.txt')
 
-console.log("this is f1 ->" + data);
+// console.log("this is f1 ->" + data);
 
 
 // write file 
@@ -19,9 +19,29 @@ fs.appendFileSync("f3.txt", " appended text ");
 console.log("f3 " + fs.readFileSync('f3.txt'));
 
 // delete a file 
-fs.unlinkSync('f2.txt');
 
-console.log('file deleted');
+// fs.unlinkSync('f2.txt');
+
+// console.log('file deleted');
 
 // -- FS MODULE FOR DIRECTORIES --- \\ -> [DIRECTORY OPERATIONS]
 
+
+//rename file
+// fs.renameSync("f1.txt", "f1(renamed).txt");
+
+console.log('renamed file successfully');
+
+
+//create , delete a directory
+// fs.mkdirSync("myDirectory");
+
+//delete a directory
+// fs.rmdirSync('myDirectory');
+
+
+//check if some directory or file exists
+const doesExist = fs.existsSync('f2.txt');
+const doesExist1 = fs.existsSync('f2.txbt');
+console.log(doesExist);
+console.log(doesExist1);

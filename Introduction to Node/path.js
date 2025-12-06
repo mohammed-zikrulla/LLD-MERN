7adbc588134e2ec7090d57041568106e77c46c76
Zik <zikrulla.zik@gmail.com>
2025-12-06 18:39:56 +0530

    copy pase file from one dir to another

--- Introduction to Node/path.js
+++ Introduction to Node/path.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 // console.log(path);
 
 // ----BASENAME AND EXTNAME----- //
@@ -23,3 +24,16 @@ console.log(__filename);
 // => to maintain paths, OS management
 
 //EXERCISE
+// copy the file from one directory and paste it inside another (myDirectory1 to myDirectory2)
+
+//procedure -> first copy the path of the source file and join its basename with the destination foleder.
+
+const srcFilePath = '/Users/zikrulla/Documents/LLD-MERN/Introduction to Node/myDirectory1/f8.txt'
+const destForlderPath = '/Users/zikrulla/Documents/LLD-MERN/Introduction to Node/myDirectory2'
+
+const srcFileBaseName = path.basename(srcFilePath);
+
+const newPath = path.join(destForlderPath, srcFileBaseName);
+
+// copy to new path we created
+fs.copyFileSync(srcFilePath, newPath);

b87763b6ec3d95613d91e24650818b8e17297749
Zik <zikrulla.zik@gmail.com>
2025-12-10 23:20:13 +0530

    Update saved data in MongoDB

--- Express intro/server/DB.js
+++ Express intro/server/DB.js
@@ -17,8 +17,11 @@ mongoose.connect(dbString).then(()=>{
         releaseData: {type: Date, default: Date.now()}
     });
 
+// Model = (mapping the collection and its related schema)
     const CourseModel = mongoose.model('course', courseSchema);
 
+
+// Function to Add the Data to its respective Model.
     async function createCourse(){
         let newCourse = new CourseModel({
             name: "java",
@@ -30,4 +33,19 @@ mongoose.connect(dbString).then(()=>{
         console.log("added successfully")
     }
 
-    createCourse();
+// Function to update the saved Data.
+   async function updateCourse(id){
+        let course = await CourseModel.findById(id);
+        if(!course){
+            console.log('course not found');
+            return;
+        }
+        course.name='Java with SQL';
+        course.rating=5;
+        course.save();
+        console.log("course updated");
+
+   }
+
+    // createCourse();
+    updateCourse('6939a58340982ab246707952');

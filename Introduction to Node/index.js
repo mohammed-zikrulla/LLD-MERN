de0b24dc2329a537c9112b8195b644dc4ae3b940
Zik <zikrulla.zik@gmail.com>
2026-01-09 23:32:59 +0530

    formatting

--- Introduction to Node/index.js
+++ Introduction to Node/index.js
@@ -1,24 +1,21 @@
-const fs = require('fs');
-
+const fs = require("fs");
 
 // read file
 // const data = fs.readFileSync('f1.txt')
 
 // console.log("this is f1 ->" + data);
 
+// write file
+fs.writeFileSync("f3.txt", "this is the new content of f3");
 
-// write file 
-fs.writeFileSync('f3.txt', "this is the new content of f3");
+console.log("f3 " + fs.readFileSync("f3.txt"));
 
-console.log("f3 " + fs.readFileSync('f3.txt'));
-
-
-// update file 
+// update file
 fs.appendFileSync("f3.txt", " appended text ");
 
-console.log("f3 " + fs.readFileSync('f3.txt'));
+console.log("f3 " + fs.readFileSync("f3.txt"));
 
-// delete a file 
+// delete a file
 
 // fs.unlinkSync('f2.txt');
 
@@ -26,12 +23,10 @@ console.log("f3 " + fs.readFileSync('f3.txt'));
 
 // -- FS MODULE FOR DIRECTORIES --- \\ -> [DIRECTORY OPERATIONS]
 
-
 //rename file
 // fs.renameSync("f1.txt", "f1(renamed).txt");
 
-console.log('renamed file successfully');
-
+console.log("renamed file successfully");
 
 //create , delete a directory
 // fs.mkdirSync("myDirectory");
@@ -39,9 +34,8 @@ console.log('renamed file successfully');
 //delete a directory
 // fs.rmdirSync('myDirectory');
 
-
 //check if some directory or file exists
-const doesExist = fs.existsSync('f2.txt');
-const doesExist1 = fs.existsSync('f2.txbt');
+const doesExist = fs.existsSync("f2.txt");
+const doesExist1 = fs.existsSync("f2.txbt");
 console.log(doesExist);
 console.log(doesExist1);

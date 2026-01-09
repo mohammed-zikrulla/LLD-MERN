c1107360632a401d3b24c7864ccf68b275a3d248
Zik <zikrulla.zik@gmail.com>
2026-01-09 23:32:52 +0530

    formatting

--- Express intro/server/server.js
+++ Express intro/server/server.js
@@ -1,5 +1,5 @@
 //npm i express
-const express = require('express')
+const express = require("express");
 
 const app = express();
 
@@ -19,64 +19,69 @@ app.use(express.json());
 // })
 
 const courses = [
-    { id: 1, name: "java" },
-    { id: 2, name: "javascript" },
-    { id: 3, name: "python" }
-]
+  { id: 1, name: "java" },
+  { id: 2, name: "javascript" },
+  { id: 3, name: "python" },
+];
 
 //---GET METHOD----//
 // get all courses
-app.get('/courses', (req, res) => {
-    res.send(courses);
-})
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
 
 // get specific value based on the parameter
-app.get('/courses/:id', (req, res) => {
-    const course = courses.find(course => course.id === Number.parseInt(req.params.id));
-    if (!course) {
-        res.status(404);
-    }
-    res.send(course);
-
-})
+app.get("/courses/:id", (req, res) => {
+  const course = courses.find(
+    (course) => course.id === Number.parseInt(req.params.id)
+  );
+  if (!course) {
+    res.status(404);
+  }
+  res.send(course);
+});
 
 //----POST----//
 
-app.post('/courses', (req, res) => {
-    const course = {
-        id: req.body.id,
-        name: req.body.name
-    }
+app.post("/courses", (req, res) => {
+  const course = {
+    id: req.body.id,
+    name: req.body.name,
+  };
 
-    courses.push(course);
+  courses.push(course);
 
-    res.send(courses);
-})
+  res.send(courses);
+});
 
 // ----PUT------//
 
-app.put('/courses/:id', (req, res) => {
-    let course = courses.find(course => Number.parseInt(req.params.id) === course.id);
-    if (!course) {
-        res.status(404).send("cannot update the course")
-    }
-    course.name = req.body.name;
-    res.send(course);
-})
+app.put("/courses/:id", (req, res) => {
+  let course = courses.find(
+    (course) => Number.parseInt(req.params.id) === course.id
+  );
+  if (!course) {
+    res.status(404).send("cannot update the course");
+  }
+  course.name = req.body.name;
+  res.send(course);
+});
 
 // --------Delete--------- //
 
-app.delete('/courses/:id', (req, res) => {
-    let course = courses.find(course => Number.parseInt(req.params.id) === course.id);
-    if (!course) {
-        res.status(404).send("cannot delete the course")
-    }
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-    res.send(course);
-})
+app.delete("/courses/:id", (req, res) => {
+  let course = courses.find(
+    (course) => Number.parseInt(req.params.id) === course.id
+  );
+  if (!course) {
+    res.status(404).send("cannot delete the course");
+  }
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.send(course);
+});
 
 //listener port(host)
 app.listen(8005, () => {
-    console.log('server started on port 8005');
-})
+  console.log("server started on port 8005");
+});
